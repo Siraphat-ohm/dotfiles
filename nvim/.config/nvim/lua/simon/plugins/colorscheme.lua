6161117cc2921c7b21e0f6dfbb50f6c12c61af9f
siraphat-ohm <siraphat975@gmail.com>
2025-12-14 19:34:12 +0700

    new colorscheme

--- nvim/.config/nvim/lua/simon/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/simon/plugins/colorscheme.lua
@@ -1,34 +1,34 @@
 return {
-  "rebelot/kanagawa.nvim",
-  priority = 1000,
-  config = function()
-    -- Default options:
-    require('kanagawa').setup({
-      compile = false,             -- enable compiling the colorscheme
-      undercurl = true,            -- enable undercurls
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true},
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = false,         -- do not set background color
-      dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-      colors = {                   -- add/modify theme and palette colors
-        palette = {},
-        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-      },
-      overrides = function(colors) -- add/modify highlights
-        return {}
-      end,
-      theme = "wave",              -- Load "wave" theme when 'background' option is not set
-      background = {               -- map the value of 'background' option to a theme
-        dark = "wave",           -- try "dragon" !
-        light = "lotus"
-      },
-    })
+    "echasnovski/mini.base16",
+    version = "*",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        -- Define your HackTheBox palette
+        -- I have mapped your JSON colors to the Base16 standard for the best result.
+        -- Note: I used the "Bright" versions of Blue/Purple/Cyan for better readability on the dark background.
+        local htb_palette = {
+            base00 = "#1a2332", -- background
+            base01 = "#313f55", -- cursorColor / selectionBackground
+            base02 = "#313f55", -- selection
+            base03 = "#666666", -- brightBlack (Comments)
+            base04 = "#a4b1cd", -- foreground (Darker text)
+            base05 = "#a4b1cd", -- foreground (Main text)
+            base06 = "#ffffff", -- white
+            base07 = "#ffffff", -- brightWhite
+            base08 = "#ff8484", -- brightRed (Variables)
+            base09 = "#ffaf00", -- yellow (Integers/Orange)
+            base0A = "#ffcc5c", -- brightYellow (Classes)
+            base0B = "#9fef00", -- green (Strings)
+            base0C = "#5cecc6", -- brightCyan (Support)
+            base0D = "#5cb2ff", -- brightBlue (Functions) - Used bright for contrast
+            base0E = "#c16cfa", -- brightPurple (Keywords) - Used bright for contrast
+            base0F = "#ff3e3e", -- red (Deprecated/Brown)
+        }
 
-    -- setup must be called before loading
-    vim.cmd("colorscheme kanagawa")
-  end
+        require("mini.base16").setup({
+            palette = htb_palette,
+            use_cterm = true, -- Use terminal colors
+        })
+    end,
 }
